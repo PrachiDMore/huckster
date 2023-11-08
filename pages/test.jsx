21af088e906a7fb6abd4832dc46d38102d0d0e9c
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2023-11-08 11:24:24 +0530

    initial commit

---
+++ pages/test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Test = () => {
+	return (
+		<>
+			<section className='relative h-screen w-screen bg-black' style={{
+			}}>
+				<img className='absolute top-0 left-0 z-0 h-screen w-screen object-cover' src="https://framerusercontent.com/images/wihlKTqe05H57msmNhuMMZIAmM.png" alt="" />
+				<div className='absolute top-0 left-0 z-10 h-screen w-screen bg-gradient-radial from-transparent to-black'  ></div>
+				<nav>
+
+				</nav>
+			</section>
+
+		</>
+	)
+}
+
+export default Test
