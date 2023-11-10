d0f57409289920f059c992d8358b37946ed8a646
Prachi More <prachimore2702@gmail.com>
2023-11-10 21:15:57 +0530

    changes

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -8,9 +8,8 @@ const syne = Syne({ subsets: ['latin'] })
 const TeamCard = () => {
   return (
     <div className='border border-white/40 rounded-xl p-3 text-center'>
-      <img className='rounded-lg w-full h-80 object-cover' src="/assets/team.webp" alt="" />
-      <p className='text-sm text-white/60 py-2'>Meet our Versatile visionary, a Filmmaker and Latex Harvest technician. He spearheads @citizen.hippie productions.</p>
-      <h1 className={'text-xl ' + syne.className}>Praveen Prabhakaram</h1>
+      <img className='rounded-lg w-full h-80 object-cover scale-105 hover:scale-100 transition duration-500' src="/assets/team.webp" alt="" />
+      <h1 className={'text-xl pt-3 ' + syne.className}>Praveen Prabhakaram</h1>
       <p className='text-white/80'>Director</p>
     </div>
   )
